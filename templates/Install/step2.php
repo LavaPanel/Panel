32b2b514bccc484bd064b699ef24d19e9cfb1dda
weeryan17 <theweeryan17@gmail.com>
2018-04-14 12:14:01 -0500

    Totally didn't forget to add this to git 👀

---
+++ templates/Install/step2.php
@@ -0,0 +1,10 @@
+<div id="step2">
+    <div class="jumbotron">
+        <h1>Storage Setup</h1>
+        <hr>
+        <p>In this step you will set up your storage method</p>
+    </div>
+    <script>
+        enableButtons(2);
+    </script>
+</div>
